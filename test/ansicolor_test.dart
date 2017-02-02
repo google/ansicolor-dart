cd1be24c40fd4ba15166a04fcc31baedf7beaa61
John Thomas McDole <john@mcdole.org>
2017-02-02 00:00:47 -0800

    Issue #10: Clean up style
    Cleanup comment style, move ansi_demo to bin/demo.dart, update to use the
    new dart test library.

--- test/ansicolor_test.dart
+++ test/ansicolor_test.dart
@@ -1,65 +1,56 @@
 library ansicolor_test;
 
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import "../lib/ansicolor.dart";
 
-class AnsiTestConfig extends SimpleConfiguration {
-  void onTestResult(TestCase testCase) {
-    color_disabled = false;
-    super.onTestResult(testCase);
-  }
-}
-
 void main() {
-  unittestConfiguration = new AnsiTestConfig();
+  tearDown(() {
+    color_disabled = false;
+  });
 
   test('foreground', () {
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: 1.0, g: 0.8, b: 0.2);
+    AnsiPen pen = new AnsiPen()..rgb(r: 1.0, g: 0.8, b: 0.2);
     expect(pen.write("Test Text"), "\x1B[38;5;221mTest Text\x1B[0m");
   });
 
   test('background', () {
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
+    AnsiPen pen = new AnsiPen()..rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
     expect(pen.write("Test Text"), "\x1B[48;5;117mTest Text\x1B[0m");
   });
 
   test('foreground and background', () {
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: 1.0, g: 0.8, b: 0.2)..
-        rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
-    expect(pen.write("Test Text"),
-        "\x1B[38;5;221m\x1B[48;5;117mTest Text\x1B[0m");
+    AnsiPen pen = new AnsiPen()
+      ..rgb(r: 1.0, g: 0.8, b: 0.2)
+      ..rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
+    expect(
+        pen.write("Test Text"), "\x1B[38;5;221m\x1B[48;5;117mTest Text\x1B[0m");
   });
 
   test('foreground and background w/ resets', () {
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: 1.0, g: 0.8, b: 0.2)..
-        rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
+    AnsiPen pen = new AnsiPen()
+      ..rgb(r: 1.0, g: 0.8, b: 0.2)
+      ..rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
     expect(pen.write("Test${resetBackground()} Text${resetForeground()}Test"),
         "\x1B[38;5;221m\x1B[48;5;117mTest\x1B[49m Text\x1B[39mTest\x1B[0m");
   });
 
   test('direct xterm', () {
-    AnsiPen pen = new AnsiPen()..
-        xterm(200)..
-        xterm(100, bg: true);
-    expect(pen.write("Test Text"),
-        "\x1B[38;5;200m\x1B[48;5;100mTest Text\x1B[0m");
+    AnsiPen pen = new AnsiPen()..xterm(200)..xterm(100, bg: true);
+    expect(
+        pen.write("Test Text"), "\x1B[38;5;200m\x1B[48;5;100mTest Text\x1B[0m");
   });
 
   test('call() == write()', () {
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: 1.0, g: 0.8, b: 0.2)..
-        rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
+    AnsiPen pen = new AnsiPen()
+      ..rgb(r: 1.0, g: 0.8, b: 0.2)
+      ..rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
     expect(pen.write("Test Text"), pen("Test Text"));
   });
 
   test('interpolated == write()', () {
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: 1.0, g: 0.8, b: 0.2)..
-        rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
+    AnsiPen pen = new AnsiPen()
+      ..rgb(r: 1.0, g: 0.8, b: 0.2)
+      ..rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
     expect("${pen}Test Text${pen.up}", pen("Test Text"));
   });
 
@@ -105,42 +96,42 @@ void main() {
   });
 
   test('rgb overflow', () {
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: 2.0, g: 2.8, b: 2.2)..
-        rgb(r: 2.4, g: 2.8, b: 2.0, bg: true);
-    expect(pen.write("Test Text"),
-        "\x1B[38;5;231m\x1B[48;5;231mTest Text\x1B[0m");
+    AnsiPen pen = new AnsiPen()
+      ..rgb(r: 2.0, g: 2.8, b: 2.2)
+      ..rgb(r: 2.4, g: 2.8, b: 2.0, bg: true);
+    expect(
+        pen.write("Test Text"), "\x1B[38;5;231m\x1B[48;5;231mTest Text\x1B[0m");
   });
 
   test('rgb underflow', () {
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: -1.0, g: -2.8, b: -2.2)..
-        rgb(r: -1.0, g: -2.8, b: -2.0, bg: true);
-    expect(pen.write("Test Text"),
-        "\x1B[38;5;16m\x1B[48;5;16mTest Text\x1B[0m");
+    AnsiPen pen = new AnsiPen()
+      ..rgb(r: -1.0, g: -2.8, b: -2.2)
+      ..rgb(r: -1.0, g: -2.8, b: -2.0, bg: true);
+    expect(
+        pen.write("Test Text"), "\x1B[38;5;16m\x1B[48;5;16mTest Text\x1B[0m");
   });
 
   test('grayscale', () {
     AnsiPen pen = new AnsiPen();
 
     for (int i = 0; i < 24; i++) {
-      expect((pen..gray(level: i / 23)).down,
-          "\x1B[38;5;${232 + i}m", reason: "fg failed at $i");
+      expect((pen..gray(level: i / 23)).down, "\x1B[38;5;${232 + i}m",
+          reason: "fg failed at $i");
     }
 
     expect((pen..reset()).down, "");
 
     for (int i = 0; i < 24; i++) {
-      expect((pen..gray(level: i / 23, bg: true)).down,
-          "\x1B[48;5;${232 + i}m", reason: "bg failed at $i");
+      expect((pen..gray(level: i / 23, bg: true)).down, "\x1B[48;5;${232 + i}m",
+          reason: "bg failed at $i");
     }
   });
 
   test('color_disabled', () {
     color_disabled = false;
-    AnsiPen pen = new AnsiPen()..
-        rgb(r: 1.0, g: 0.8, b: 0.2)..
-        rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
+    AnsiPen pen = new AnsiPen()
+      ..rgb(r: 1.0, g: 0.8, b: 0.2)
+      ..rgb(r: 0.4, g: 0.8, b: 1.0, bg: true);
     color_disabled = true;
     expect(pen.write("Test Text"), "Test Text");
   });
